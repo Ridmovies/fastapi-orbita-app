e86c654197bea9294bbb9e1e8a658e2177bd0848
riddler_rid <unkownrid@gmail.com>
2025-01-23 13:09:31 +0300

    add post tests

--- tests/test_post_app.py
+++ tests/test_post_app.py
@@ -1,7 +1,7 @@
 import pytest
 from httpx import AsyncClient
 
-auth_prefix = f"/api/v1/auth"
+# auth_prefix = f"/api/v1/auth"
 
 @pytest.mark.asyncio
 async def test_user_creation(client):
@@ -11,12 +11,23 @@ async def test_user_creation(client):
 
 
 @pytest.mark.asyncio
-async def test_abc(client):
-    assert 1 == 1
-
-
-@pytest.mark.asyncio
 async def test_get_all_posts(client: AsyncClient):
     response = await client.get("/posts")
     assert response.status_code == 200
     assert len(response.json()) == 0
+
+
+@pytest.mark.asyncio
+async def test_create_post(client: AsyncClient):
+    post_data = {"content": "pytest_post_1"}
+    response = await client.post("/posts", json=post_data)
+    assert response.status_code == 200
+    assert response.json() == {"content": "pytest_post_1", "id": 1}
+
+
+@pytest.mark.asyncio
+async def test_get_post_by_id(client: AsyncClient):
+    post_id = 1
+    response = await client.get(f"/posts/{post_id}")
+    assert response.status_code == 200
+    assert response.json() == {"content": "pytest_post_1", "id": 1}
